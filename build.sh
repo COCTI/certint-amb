8bf1395cbfc013a38316f12a5ed25955a2a41452
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2021-05-23 21:57:30 +0900

    remove broken files and update makefiles

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
+#!/bin/sh
 set -e
 wait="$1"
+args="-R . StructPoly"
 makeit() {
  for i in "$@"; do
    if test "$wait" = "$i"; then wait=""; fi
-   if test -z "$wait"; then echo coqc "$i" ; coqc "$i" ; fi
+   if test -z "$wait"; then echo coqc $args "$i.v" ; coqc $args "$i.v" ; fi
  done; }
-makeit Lib_Tactic Lib_ListFacts Lib_ListFactsMore
-makeit Lib_MyFSetInterface Lib_FinSet Lib_FinSetImpl
+makeit FSetList Lib_Tactic Lib_MyFSetInterface Lib_FinSet Lib_ListFacts
+makeit Lib_FinSetImpl Lib_ListFactsMore
 makeit Metatheory_Var Metatheory_Fresh Metatheory_Env
 makeit Metatheory_SP Metatheory
 makeit ML_SP_Definitions ML_SP_Infrastructure
-makeit ML_SP_Soundness ML_SP_Rename ML_SP_Eval
-makeit Cardinal ML_SP_Unify_wf ML_SP_Inference_wf
-makeit ML_SP_Domain
+makeit ML_SP_Soundness
--- mkmakefile.sh
+++ mkmakefile.sh
@@ -1,2 +1,3 @@
-coq_makefile -R . StructPoly -install none FSetList.v Lib*.v Metatheory*.v ML_SP*.v Cardinal.v Extraction.v -o Makefile
-
+#!/bin/sh
+set +x
+echo coq_makefile -R . StructPoly -install none FSetList.v Lib*.v Metatheory*.v ML_SP*.v -o Makefile
